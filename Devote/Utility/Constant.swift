ac3f1a68ea8b12d63f4601985ea7db0b307e89d5
Mohmed Anis Agwan <anishagwan123@gmail.com>
2021-06-18 19:17:31 +0530

    Setup the Core data files and settings

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Anis on 18/06/21.
+//
+
+import SwiftUI
+
+//MARK:- Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK:- UI
+
+
+//MARK:- UX
